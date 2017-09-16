2be9defadc4494122fc754c7c11027c4088f4ec4
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-16 11:30:14 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+
+const tracks = [{ title: 'Track-1' }, { title: 'Track-2' }, { title: 'Track-3' }, { title: 'Track-4' }];
+
+ReactDOM.render(<Stream tracks={tracks} />, document.getElementById('root'));
