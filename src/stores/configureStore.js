eda194b1fdba35f75451d204dc833c4516e9d40c
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-16 16:14:31 +0300

    refactor store

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,11 +1,29 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import rootReducer from '../reducers/index';
+import rootReducer from '../reducers/';
 
-const logger = createLogger();
+const middleware = [];
 
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
-
-export default function configureStore(initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
 }
+
+const configureStore = preLoadedState => {
+  const store = createStore(
+    rootReducer,
+    preLoadedState,
+    applyMiddleware(...middleware)
+  );
+
+  // Enabling Webpack for HMR for reducers ;)
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers').default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+};
+
+export default configureStore;
